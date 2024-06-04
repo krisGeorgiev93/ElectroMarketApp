0ead83c5e1cf13b09ca87581d6eedf2a42ccb313
Kristiyan Georgiev <krisgeorgiev027@gmail.com>
2024-06-04 20:43:48 +0100

    update

---
+++ ElectroMarket/ElectroMarket.Data/Migrations/20240604193413_updateDb.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ElectroMarket.Data.Migrations
+{
+    public partial class updateDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

d572928b3068af49637eb489db9c2d1fc4aa5262
Kristiyan Georgiev <krisgeorgiev027@gmail.com>
2024-06-15 21:43:15 +0100

    update pagination

--- ElectroMarket/ElektroMarket.Web.ViewModels/Product/PaginationViewModel.cs
+++ ElectroMarket/ElektroMarket.Web.ViewModels/Product/PaginationViewModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ElektroMarket.Web.ViewModels.Product
+﻿namespace ElektroMarket.Web.ViewModels.Product
 {
     public class PaginationViewModel
     {
